fd7391bfdc3e1ba293b57a3f37851c0e4214017f
Ashwin Arora <ashwinarora48@gmail.com>
2023-09-21 19:32:22 +0530

    returning TransactionView struct

--- contracts/GovernorState.sol
+++ contracts/GovernorState.sol
@@ -22,6 +22,14 @@ contract GovernorState {
         mapping(address => bool) voteOf;
     }
 
+    struct TransactionView {
+        address destination;
+        uint value;
+        bytes data;
+        bool executed;
+        uint8 totalVotes;
+    }
+
     event FunctionAdded(address indexed _contract, bytes4 indexed _selector, uint8 _requiredVotes);
     event FunctionRemoved(address indexed _contract, bytes4 indexed _selector);
     event FunctionGranted(address indexed _contract, bytes4 indexed _selector, address indexed _user);
@@ -30,21 +38,16 @@ contract GovernorState {
     event TransactionApproved(uint txId, address indexed _destination, uint8 votes);
     event TransactionExecuted(uint txId, address indexed _destination, uint _value, bytes _data);
 
-    function getTransactionById(uint _txId) external view returns (
-        address destination,
-        uint value,
-        bytes memory data,
-        uint8 totalVotes,
-        bool executed
-    ) {
-        Transaction storage transaction = transactions[_txId];
-        destination = transaction.destination;
-        value = transaction.value;
-        data = transaction.data;
-        totalVotes = transaction.votes.total;
-        executed = transaction.executed;
+    function getTransactionById(uint _txId) external view returns (TransactionView memory transaction) {
+        Transaction storage _tx = transactions[_txId];
+        transaction = TransactionView(
+            _tx.destination,
+            _tx.value,
+            _tx.data,
+            _tx.executed,
+            _tx.votes.total
+        );
     }
-    
 
     function getVoteOfTransactionById(uint _txId, address _user) external view returns (bool) {
         return transactions[_txId].votes.voteOf[_user];
